ae490d710dd14f035f89df5be31d2b1c719b6b9c
zmzlois <lois.z@comcord.vision>
2024-08-11 02:52:26 +0100

    init

--- modern.config.ts
+++ modern.config.ts
@@ -1,8 +1,8 @@
-import { appTools } from '@modern-js/app-tools';
+import { appTools, defineConfig } from '@modern-js/app-tools';
 import { tailwindcssPlugin } from '@modern-js/plugin-tailwindcss';
 import { withZephyr } from 'zephyr-webpack-plugin';
 // https://modernjs.dev/en/configure/app/usage
-export default withZephyr()({
+export default defineConfig({
   // @ts-expect-error this exists
   runtime: {
     router: true,
