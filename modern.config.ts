5e0f26d036b448a8395f7563eb5b604c3a4f6615
zmzlois <lois.z@comcord.vision>
2024-08-12 13:02:58 +0100

    development branch

--- modern.config.ts
+++ modern.config.ts
@@ -3,10 +3,6 @@ import { tailwindcssPlugin } from '@modern-js/plugin-tailwindcss';
 import { withZephyr } from 'zephyr-webpack-plugin';
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
-  html: {
-    disableHtmlFolder: true,
-  },
-
   runtime: {
     router: true,
     state: true,
