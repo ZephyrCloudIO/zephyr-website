699726504ffea7f4c829513a9f347a1266ec48a6
Néstor <nestor@nstlopez.com>
2025-01-27 14:11:47 +0100

    chore: add Google analytics

--- src/Document.tsx
+++ src/Document.tsx
@@ -1,24 +1,32 @@
-import React, { useContext } from 'react';
-import {
-  Html,
-  Root,
-  Head,
-  Body,
-  Comment,
-  DocumentContext,
-} from '@modern-js/runtime/document';
+import React from 'react';
+import { Html, Root, Head, Body } from '@modern-js/runtime/document';
+
+const GoogleAnalytics = () => (
+  <>
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=G-B7G266JZDH`}
+    />
+    <script
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-B7G266JZDH');
+      `,
+      }}
+    />
+  </>
+);
 
 export default function Document(): React.ReactElement {
-  const {
-    config: { output: htmlConfig },
-    entryName,
-    templateParams,
-  } = useContext(DocumentContext);
-
   return (
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
+        <GoogleAnalytics />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes"
