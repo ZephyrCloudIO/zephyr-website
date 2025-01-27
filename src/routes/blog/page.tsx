30fac7831f2bbbaff4efea2def44bd181eeed09d
Zack Chapple <zackarychapple30@gmail.com>
2025-01-27 10:50:19 -0500

    feat(blog): adding launch week 2 kickoff

--- src/routes/blog/page.tsx
+++ src/routes/blog/page.tsx
@@ -14,48 +14,55 @@ const blogPosts: BlogPost[] = [
     title: 'Infrastructureless Future',
     slug: './infrastructureless',
     date: '2024-07-18',
-    description: 'Serverless computing has been hailed as a groundbreaking shift in web infrastructure. Yet, the term is somewhat misleading.'
-  }
+    description:
+      'Serverless computing has been hailed as a groundbreaking shift in web infrastructure. Yet, the term is somewhat misleading.',
+  },
+  {
+    title: 'Launch Week 2: Kickoff',
+    slug: './mobilefirst',
+    date: '2025-01-27',
+    description: 'Kicking off our second launch week with a new theme!',
+  },
 ];
 
 const BlogPage: FC = () => {
   return (
     <>
-    <div className="absolute inset-x-0 top-0 h-[600px] overflow-hidden mt-16">
-      <div className="absolute -translate-x-[240px] w-[800px] opacity-90">
-        <img
-          src={JesusBeam}
-          className="w-full"
-          style={{
-            filter: 'blur(50px) brightness(5)',
-            WebkitFilter: 'blur(50px) brightness(5)',
-          }}
-          alt=""
-        />
+      <div className="absolute inset-x-0 top-0 h-[600px] overflow-hidden mt-16">
+        <div className="absolute -translate-x-[240px] w-[800px] opacity-90">
+          <img
+            src={JesusBeam}
+            className="w-full"
+            style={{
+              filter: 'blur(50px) brightness(5)',
+              WebkitFilter: 'blur(50px) brightness(5)',
+            }}
+            alt=""
+          />
+        </div>
+        <div className="absolute w-[800px] opacity-90">
+          <img
+            src={JesusBeam}
+            className="w-full"
+            style={{
+              filter: 'blur(50px) brightness(5)',
+              WebkitFilter: 'blur(50px) brightness(5)',
+            }}
+            alt=""
+          />
+        </div>
+        <div className="absolute translate-x-[240px] w-[800px] opacity-90">
+          <img
+            src={JesusBeam}
+            className="w-full"
+            style={{
+              filter: 'blur(50px) brightness(5)',
+              WebkitFilter: 'blur(50px) brightness(5)',
+            }}
+            alt=""
+          />
+        </div>
       </div>
-      <div className="absolute w-[800px] opacity-90">
-        <img
-          src={JesusBeam}
-          className="w-full"
-          style={{
-            filter: 'blur(50px) brightness(5)',
-            WebkitFilter: 'blur(50px) brightness(5)',
-          }}
-          alt=""
-        />
-      </div>
-      <div className="absolute translate-x-[240px] w-[800px] opacity-90">
-        <img
-          src={JesusBeam}
-          className="w-full"
-          style={{
-            filter: 'blur(50px) brightness(5)',
-            WebkitFilter: 'blur(50px) brightness(5)',
-          }}
-          alt=""
-        />
-      </div>
-    </div>
       <div className="container relative max-w-4xl mx-auto px-4 py-12">
         <h1 className="text-3xl sm:text-4xl md:text-5xl p-4 font-bold text-center m-6 md:mb-12 bg-gradient-to-r from-white to-[#808080] bg-clip-text text-transparent tracking-wider">
           Blogs
@@ -78,7 +85,7 @@ const BlogPage: FC = () => {
                     {new Date(post.date).toLocaleDateString('en-US', {
                       year: 'numeric',
                       month: 'long',
-                      day: 'numeric'
+                      day: 'numeric',
                     })}
                   </div>
                   <p className="text-gray-300 line-clamp-3">
