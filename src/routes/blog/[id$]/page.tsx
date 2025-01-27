12d7e071550d87a763250de3602eab0ca7cc78fe
Russell Canfield <rcanfield86@gmail.com>
2025-01-27 14:40:40 -0500

    Import

--- src/routes/blog/[id$]/page.tsx
+++ src/routes/blog/[id$]/page.tsx
@@ -5,6 +5,7 @@ import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
 import bash from 'highlight.js/lib/languages/bash';
+import { MDXLayout } from '@/components/mdx-wrapper';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('typescript', typescript);
