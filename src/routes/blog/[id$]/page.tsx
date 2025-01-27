f35109b28a381b4a39ff3442832e72cdff3d7edf
Néstor <nestor@nstlopez.com>
2025-01-27 19:43:11 +0100

    feat: fix images

--- src/routes/blog/[id$]/page.tsx
+++ src/routes/blog/[id$]/page.tsx
@@ -1,6 +1,6 @@
-import { MDXLayout } from '@/components/mdx-wrapper';
 import { useParams } from '@modern-js/runtime/router';
 import { Suspense, useMemo, lazy } from 'react';
+import { MDXLayout } from '@/components/mdx-wrapper';
 
 type MDXComponent = {
   default: () => JSX.Element;
@@ -10,7 +10,9 @@ export default function BlogPost() {
   const { id } = useParams<{ id: string }>();
 
   const BlogMDX = useMemo(() => {
-    if (!id) return null;
+    if (!id) {
+      return null;
+    }
 
     return lazy(() => import(`./${id}.mdx`) as Promise<MDXComponent>);
   }, [id]);
@@ -19,7 +21,6 @@ export default function BlogPost() {
 
   return (
     <div className="flex flex-col gap-2 px-2 py-10 md:px-10">
-      {' '}
       <MDXLayout>
         <Suspense fallback={<div>Loading...</div>}>
           {BlogMDX && <BlogMDX />}
