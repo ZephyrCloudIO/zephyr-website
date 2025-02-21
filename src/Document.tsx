2585f0d7e39a0fe699c4a4773555f6e8ac008c69
zmzlois <lois.z@comcord.vision>
2025-02-21 18:23:32 +0000

    init

--- src/Document.tsx
+++ src/Document.tsx
@@ -13,8 +13,29 @@ const GoogleAnalytics = () => (
         __html: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-B7G266JZDH');
+        gtag('js', new Date()); 
+        const id = '*_ze_id'; 
+
+        function find_or_create_ze_id() { 
+
+const user_id = localStorage.getItem(id); 
+
+if (!user_id) { 
+  localStorage.setItem(id, Math.random().toString(36).substring(2, 15)); 
+}
+
+      }
+        gtag('config', 'G-B7G266JZDH', {
+        page_path: window.location.pathname,
+            first_field_name: 'website',
+            linker: {
+              domains: ['app.zephyr-cloud.io', 'zephyr-cloud.io', 'docs.zephyr-cloud.io']
+            } 
+        });
+      gtag('set', 'linker', {
+      accept_incoming: true,
+    }
+       
       `,
       }}
     />
