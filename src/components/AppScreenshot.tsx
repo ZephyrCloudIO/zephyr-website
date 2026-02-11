c6145f414ff5070253c42a26c218784eca3caca0
Néstor <nestor@nstlopez.com>
2026-02-11 12:04:19 +0100

    chore: fix width for mobile

--- src/components/AppScreenshot.tsx
+++ src/components/AppScreenshot.tsx
@@ -9,7 +9,7 @@ export function AppScreenshot({ shaderReady }: { shaderReady: boolean }) {
     <img
       src={appMockup}
       alt="Zephyr app interface"
-      className="tilt-in -mt-2 h-auto w-[calc(100%+104px)] max-w-none -mx-[52px] drop-shadow-[0_40px_80px_rgba(0,0,0,0.8)] md:-mt-3"
+      className="tilt-in -mt-1 h-auto w-full max-w-[980px] drop-shadow-[0_40px_80px_rgba(0,0,0,0.8)] md:-mt-3 md:w-[calc(100%+104px)] md:max-w-none md:-mx-[52px]"
       style={{
         opacity: ready ? 1 : 0,
         ...(ready && { transition: 'opacity 0.6s ease-out 0.15s' }),
